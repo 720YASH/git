711f297bce931028e1662a8108597d1ba6cc4263
root <root@ip-172-31-37-19.ap-southeast-2.compute.internal>
2022-09-23 15:38:08 +0000

    rewritten addition java

--- addition.java
+++ addition.java
@@ -2,3 +2,16 @@ int x = 5;
 int y = 6;
 int sum = x + y;
 System.out.println(sum); // Print the sum of x + y
+public class NumberAddition{
+   public static void main(String[] args){
+      int value_1, value_2, my_sum;
+      value_1 = 10;
+      value_2 = 15;
+      System.out.printf("The two numbers are %d and %d",value_1, value_2 );
+      System.out.printf("\n");
+      my_sum = value_1 + value_2;
+      System.out.println("The numbers have been added using '+' operator");
+      System.out.println("\nSum of the two numbers is : ");
+      System.out.println(my_sum);
+   }
+}
